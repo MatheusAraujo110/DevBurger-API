745d0f6e09f44d1c8ef7c58f9517ca07a6b38721
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-09-29 22:29:49 -0300

    configurando model de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { v4 } from "uuid"
+
+import User from './app/models/User'
 
 const router = new Router()
 
-router.get('/', (request, response) => {
-    return response.status(200).json({ message: 'Funcionando noraml...' })
+router.get('/', async (request, response) => {
+    const user = await User.create({
+        id: v4(),
+        name: 'matheus',
+        email: 'theusnascimento386@gmail.com',
+        password_hash: '25961511',
+    })
+
+    return response.status(201).json(user)
 })
 
 export default router
